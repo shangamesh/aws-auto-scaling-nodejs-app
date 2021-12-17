e7bea4db49a0faed247d4459c7eea80bc79f113d
Shangamesh <46888560+shangamesh@users.noreply.github.com>
2021-12-17 13:07:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var os = require("os");
+var hostname = os.hostname();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
+  res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>' + hostname);
 })
 
 app.listen(port, () => {
